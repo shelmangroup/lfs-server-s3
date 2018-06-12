a59b030535a26f20d006957c9899e8f63d0c6503
Lars Sjöström <lars@radicore.se>
2018-06-12 16:59:37 +0200

    update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-export AWS_ACCESS_KEY_ID=reducted
-export AWS_SECRET_ACCESS_KEY=reducted
+docker run --name minio -d -p 9000:9000 -e MINIO_ACCESS_KEY=minio -e MINIO_SECRET_KEY=miniostorage minio/minio server /data
+export AWS_ACCESS_KEY_ID=minio
+export AWS_SECRET_ACCESS_KEY=miniostorage
 export AWS_ENDPOINT=http://127.0.0.1:9000
 export AWS_BUCKET=test
 export AWS_REGION=eu-west-1
