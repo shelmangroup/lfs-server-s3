5d675b4eccc09c423c25c9314669dab9b20bfe04
Lars Sjöström <lars@radicore.se>
2018-06-12 20:52:20 +0200

    use env for bucket

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ export AWS_BUCKET=test
 export AWS_REGION=eu-west-1
 export LFS_ADMINUSER=admin
 export LFS_ADMINPASS=admin
-aws --endpoint-url http://127.0.0.1:9000 s3 mb s3://test
+aws --endpoint-url http://127.0.0.1:9000 s3 mb s3://$AWS_BUCKET
 ./lfs-test-server
